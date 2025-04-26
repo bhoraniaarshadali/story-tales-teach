02b0747cb367776d3e7e268ead12cf349068698f
Arshad ali bhorania <bhoraniyaarshadali431@gmail.com>
2025-04-26 08:25:39 +0530

    add speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { AccessibilityProvider } from "./contexts/AccessibilityContext";
@@ -18,6 +18,7 @@ const App = () => (
         <AccessibilityProvider>
           <Toaster />
           <Sonner />
+          <SpeedInsights />
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
